66ae972fc877c4ede3790e66eb4b6c34b9ee4fe6
alijaber2004 <119969405+alijaber2004@users.noreply.github.com>
2023-12-03 16:28:29 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #Data Science Tools and Ecosystem
+
+# # Data Science tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:
+# - python
+# - r
+# - c++
+# - javascript
+# - anaconda
+
+# Some of the popular languages that Data Scientists use are:
+# 1.python
+# 2.R
+# 3.javascript
+
+# 1.ali 2.jaber
+# 
+
+# Some of the popular languages that Data Scientists use are:
+#         1. python
+#         2. R
+#         3. JavaScript
+
+# Some of the popular languages that Data Scientists use are: 1. python
+#     1. R 1. JavaScript
+
+# Some of the popular languages that Data Scientists use are: 1. python
+# 1. R 
+# 1. JavaScript
+
+# Some of the popular languages that Data Scientists use are
+# 1. python
+# 1. R  
+# 1. JavaScript
+
+# Some of the commonly used libraries by Data Scientists include:
+# 1. ggplot
+# 1. Numpy
+# 1. panadas
+
+# | Data Science Tools |
+# | ------------------ |
+# | Vscode |
+# | anaconda |
+# | rstudio |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+print('This a simple arithmetic expression to mutiply then add integers')
+(3*4)+5
+
+
+# In[4]:
+
+
+print('This will convert 200 minutes to hours by diving by 60')
+(200/60)
+
+
+# ## Author 
+# ali jaber
+
+# In[ ]:
+
+
+
+
